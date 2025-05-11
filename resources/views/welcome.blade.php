24e71532a627514f29e062b3da5504ec5ebd6559
abdalhababseh <abdalhababseh@outlook.com>
2025-05-12 01:02:24 +0300

    Add particles.js script to welcome page for enhanced visual effects

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,6 +56,7 @@
     <meta name="geo.position" content="37.09024;-95.712891">
     <meta name="ICBM" content="37.09024, -95.712891">
     <script src="{{ asset('home/home.min.js') }}"></script>
+    <script src="{{ asset('home/particles.min.js') }}"></script>
     <!-- Structured Data (JSON-LD) -->
     <script type="application/ld+json">
 {
